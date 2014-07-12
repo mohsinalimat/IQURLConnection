03d896c57d53f1859d11f08d32307fe1c339450e
hackiftekhar <hack.iftekhar@gmail.com>
2014-07-12 20:46:56 +0530

    Headers updated

--- IQURLConnectionDemo/IQURLConnectionDemoTests/IQURLConnectionDemoTests.m
+++ IQURLConnectionDemo/IQURLConnectionDemoTests/IQURLConnectionDemoTests.m
@@ -2,7 +2,7 @@
 //  IQURLConnectionDemoTests.m
 //  IQURLConnectionDemoTests
 //
-//  Created by Canopus 4 on 03/02/14.
+//  Created by Iftekhar 4 on 03/02/14.
 //  Copyright (c) 2014 Iftekhar. All rights reserved.
 //
 
